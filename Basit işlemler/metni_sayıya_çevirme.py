a0b117878a6b4343fbb650504e8cb0759e389a39
mhmmdzyrt <121378130+mhmmdzyrt@users.noreply.github.com>
2026-06-03 14:39:39 +0300

    Update metni_sayıya_çevirme.py

--- Basit işlemler/metni_sayıya_çevirme.py
+++ Basit işlemler/metni_sayıya_çevirme.py
@@ -1,3 +1,5 @@
+# python
+
 kelime_sayı = {
     "bir": 1, "iki": 2, "üç": 3, "dört": 4, "beş": 5, "altı": 6, "yedi": 7, "sekiz": 8, "dokuz": 9, "on": 10,
     "yirmi": 20, "otuz": 30, "kırk": 40, "elli": 50, "altmış": 60, "yetmiş": 70, "seksen": 80, "doksan": 90,
