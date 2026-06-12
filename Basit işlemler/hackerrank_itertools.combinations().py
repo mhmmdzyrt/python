c76f5a1ff9349ea3d1c54081096653a742fa4b43
mhmmdzyrt <121378130+mhmmdzyrt@users.noreply.github.com>
2026-06-12 15:39:13 +0300

    Add comment to sorted list append line

--- Basit işlemler/hackerrank_itertools.combinations().py
+++ Basit işlemler/hackerrank_itertools.combinations().py
@@ -8,6 +8,6 @@ for _ in range(1, int(s[1]) + 1):
     comb_list = list(combinations(s[0], _))
     sorted_list = []
     for i in comb_list:
-        sorted_list.append(''.join(sorted(i)))
+        sorted_list.append(''.join(sorted(i)))  # python
     sorted_list.sort()
     [print(i) for i in sorted_list]
