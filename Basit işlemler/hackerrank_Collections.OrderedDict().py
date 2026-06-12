7ab361afc15c48505e69e961f2e8c4e9b818c57a
mhmmdzyrt <121378130+mhmmdzyrt@users.noreply.github.com>
2026-06-12 15:38:58 +0300

    Fix indentation for item_names check in OrderedDict

--- Basit işlemler/hackerrank_Collections.OrderedDict().py
+++ Basit işlemler/hackerrank_Collections.OrderedDict().py
@@ -6,7 +6,7 @@ order_dict = OrderedDict()
 for _ in range(int(input())):
     items = input().split()
     item_names = ' '.join(items[:-1])
-    if item_names in order_dict:
+    if item_names in order_dict:  # python
         order_dict[item_names] += int(items[-1])
     else:
         order_dict[item_names] = int(items[-1])
