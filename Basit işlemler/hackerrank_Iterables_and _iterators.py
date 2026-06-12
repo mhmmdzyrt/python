a9e62ace4105d4f7027da4cb92f585abec17460b
mhmmdzyrt <121378130+mhmmdzyrt@users.noreply.github.com>
2026-06-12 15:39:04 +0300

    Clarify comb_list creation with a comment
    Added a comment to clarify the purpose of comb_list.

--- Basit işlemler/hackerrank_Iterables_and _iterators.py
+++ Basit işlemler/hackerrank_Iterables_and _iterators.py
@@ -4,7 +4,7 @@ from itertools import combinations
 len_string = int(input())
 string = input().split()
 split_string = int(input())
-comb_list = list(combinations(string, split_string))
+comb_list = list(combinations(string, split_string))  # python
 count = 0
 for i in comb_list:
     for j in i:
