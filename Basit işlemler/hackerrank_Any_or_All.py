9cc1c67d7480625f2f485a186bdf428d3904368c
mhmmdzyrt <121378130+mhmmdzyrt@users.noreply.github.com>
2026-06-16 13:23:23 +0300

    Update hackerrank_Any_or_All.py

--- Basit işlemler/hackerrank_Any_or_All.py
+++ Basit işlemler/hackerrank_Any_or_All.py
@@ -1,5 +1,5 @@
 # python
-
+  # python
 # 3 lines of code
 m, number_list = int(input()), list(map(int, input().split()))  # python
 number_polid, number_postive = [0 < i == int(str(i)[::-1]) for i in number_list], [i > 0 for i in number_list]  # burasi onemmli
