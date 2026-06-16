894b1b90a33b355d21658993a2fcb89ab2793b3f
mhmmdzyrt <121378130+mhmmdzyrt@users.noreply.github.com>
2026-06-16 13:21:55 +0300

    Update hackerrank_collections.Counter().py

--- Basit işlemler/hackerrank_collections.Counter().py
+++ Basit işlemler/hackerrank_collections.Counter().py
@@ -8,7 +8,7 @@ money_earned = 0
 for _ in range(int(input())):  # python
     order = list(map(int, input().split()))
     if shoe_list[order[0]] > 0:
-        shoe_list[order[0]] -= 1
+        shoe_list[order[0]] -= 1  # python
         money_earned += order[1]
     if shoe_list[order[0]] == 0:
         continue
