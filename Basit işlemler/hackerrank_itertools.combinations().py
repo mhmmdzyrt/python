7f6453c1b011651f4af3cbe6f74aa08e35793cf3
mhmmdzyrt <121378130+mhmmdzyrt@users.noreply.github.com>
2026-06-16 13:22:15 +0300

    Update hackerrank_itertools.combinations().py

--- Basit işlemler/hackerrank_itertools.combinations().py
+++ Basit işlemler/hackerrank_itertools.combinations().py
@@ -5,7 +5,7 @@ from itertools import combinations
 s = list(input().split())
 
 for _ in range(1, int(s[1]) + 1):
-    comb_list = list(combinations(s[0], _))
+    comb_list = list(combinations(s[0], _))  # python
     sorted_list = []
     for i in comb_list:
         sorted_list.append(''.join(sorted(i)))  # python
