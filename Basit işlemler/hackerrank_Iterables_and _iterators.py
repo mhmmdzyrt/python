eaab860bb8e8dd6afc79680e0d3cabd39d4a8b21
mhmmdzyrt <121378130+mhmmdzyrt@users.noreply.github.com>
2026-06-16 13:21:10 +0300

    Update hackerrank_Iterables_and _iterators.py

--- Basit işlemler/hackerrank_Iterables_and _iterators.py
+++ Basit işlemler/hackerrank_Iterables_and _iterators.py
@@ -7,7 +7,7 @@ split_string = int(input())
 comb_list = list(combinations(string, split_string))  # python
 count = 0
 for i in comb_list:
-    for j in i:
+    for j in i:  # python
         if j == 'a':
             count += 1
             break
